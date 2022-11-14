24581cee21ed90c50216ece47cb7cd446b39bb27
= <lucasdossantos.ti@gmail.com>
2022-11-13 21:28:13 -0300

    Criando classe Usuario

---
+++ app/src/main/java/app/financaspessoais/model/Usuario.java
@@ -0,0 +1,74 @@
+package app.financaspessoais.model;
+
+import java.util.Date;
+
+public class Usuario {
+    private Integer id;
+    private String nome;
+    private String sobrenome;
+    private Date dataNascimento;
+    private String email;
+    private String senha;
+    private Date dataCadastro;
+
+    public Usuario(String nome, String sobrenome, Date dataNascimento, String email, String senha) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.dataNascimento = dataNascimento;
+        this.email = email;
+        this.senha = senha;
+        this.dataCadastro = new Date();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+}
