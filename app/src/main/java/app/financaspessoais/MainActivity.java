56a9e787ca734b6f598f779df79cd96eb6417600
= <lucasdossantos.ti@gmail.com>
2022-11-21 21:38:31 -0300

    Utilizando o View Binding

--- app/src/main/java/app/financaspessoais/MainActivity.java
+++ app/src/main/java/app/financaspessoais/MainActivity.java
@@ -2,13 +2,22 @@ package app.financaspessoais;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import app.financaspessoais.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
