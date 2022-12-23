fd3f185f4c09127e3c2cc05d0af0aa13ae4e7986
Lucas dos Santos <lucas.santos@showtecnologia.com>
2022-12-23 11:51:09 -0300

    Transformando o aplicativo de finanças pessoais em um aplicativo de notas

---
+++ app/src/main/java/app/financaspessoais/ui/activity/NotaActivityConstantes.java
@@ -0,0 +1,10 @@
+package app.financaspessoais.ui.activity;
+
+public interface NotaActivityConstantes {
+
+    String CHAVE_NOTA = "nota";
+    int CODIGO_REQUISICAO_INSERE_NOTA = 1;
+    int CODIGO_REQUISICAO_ALTERA_NOTA = 2;
+    int POSICAO_INVALIDA = -1;
+    String CHAVE_POSICAO = "posicao";
+}
---
+++ app/src/main/java/app/financaspessoais/ui/recyclerview/adapter/listener/OnItemClickListener.java
@@ -0,0 +1,8 @@
+package app.financaspessoais.ui.recyclerview.adapter.listener;
+
+import app.financaspessoais.model.Nota;
+
+public interface OnItemClickListener {
+
+    void onItemClick(Nota nota, int posicao);
+}
